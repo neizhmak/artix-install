4a4f0db51a0b37755b244402ff2d03d28874847b
ArtemNeizhmak <65288888+ArtemNeizhmak@users.noreply.github.com>
2023-04-03 14:43:19 +0000

    formatting corrections
    Formatting corrections in sh scripts to make code more readable and get rid of extra spaces in configuration files

--- 1-installer.sh
+++ 1-installer.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
 # format disk
- mkfs.ext4 -L ROOT /dev/sda1
- mkfs.ext4 -L HOME /dev/sda2
+mkfs.ext4 -L ROOT /dev/sda1
+mkfs.ext4 -L HOME /dev/sda2
 
 # mount disk
- mount /dev/disk/by-label/ROOT /mnt
- mkdir /mnt/home
- mount /dev/disk/by-label/HOME /mnt/home
+mount /dev/disk/by-label/ROOT /mnt
+mkdir /mnt/home
+mount /dev/disk/by-label/HOME /mnt/home
 
 # install system
- basestrap /mnt base dinit linux linux-firmware grub os-prober dhcpcd wpa_supplicant wpa_supplicant-dinit
+basestrap /mnt base dinit linux linux-firmware grub os-prober dhcpcd wpa_supplicant wpa_supplicant-dinit
  
- fstabgen -U /mnt >> /mnt/etc/fstab
- cp ./2-installer.sh /mnt/
- artix-chroot /mnt /2-installer.sh
+fstabgen -U /mnt >> /mnt/etc/fstab
+cp ./2-installer.sh /mnt/
+artix-chroot /mnt /2-installer.sh
 
 # finish up
- umount -R /mnt
- poweroff
+umount -R /mnt
+poweroff
--- 2-installer.sh
+++ 2-installer.sh
@@ -1,67 +1,70 @@
 #!/bin/sh
 
 # time configuration
- ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
- hwclock --systohc
+ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
+hwclock --systohc
 
 # locale configuration
- echo -e " en_US.UTF-8\n" >> /etc/locale.gen
- locale-gen
- echo -e " export LANG="en_US.UTF-8"\n export LC_COLLATE="C"\n" >> /etc/locale.conf
+echo "en_US.UTF-8" >> /etc/locale.gen
+locale-gen
+echo "export LANG="en_US.UTF-8"
+export LC_COLLATE="C"" >> /etc/locale.conf
 
 # grub configuration
- grub-install --recheck /dev/sda
- grub-mkconfig -o /boot/grub/grub.cfg
+grub-install --recheck /dev/sda
+grub-mkconfig -o /boot/grub/grub.cfg
 
 # user configuration
- passwd
- useradd -m user
- passwd user
+passwd
+useradd -m user
+passwd user
 
 # network configuration
- echo -e " desktop\n" >> /etc/hostname
- echo -e " 127.0.0.1        localhost\n ::1              localhost\n 127.0.1.1        desktop.localdomain  desktop\n" >> /etc/hosts
+echo "desktop" >> /etc/hostname
+echo "127.0.0.1        localhost
+::1              localhost
+127.0.1.1        desktop.localdomain  desktop" >> /etc/hosts
 
 # repo configuration
- echo -e '
- [universe]
- Server = https://universe.artixlinux.org/$arch
- Server = https://mirror1.artixlinux.org/universe/$arch
- Server = https://mirror.pascalpuffke.de/artix-universe/$arch
- Server = https://artixlinux.qontinuum.space/artixlinux/universe/os/$arch
- Server = https://mirror1.cl.netactuate.com/artix/universe/$arch
- Server = https://ftp.crifo.org/artix-universe/$arch
- Server = https://artix.sakamoto.pl/universe/$arch
+echo '
+[universe]
+Server = https://universe.artixlinux.org/$arch
+Server = https://mirror1.artixlinux.org/universe/$arch
+Server = https://mirror.pascalpuffke.de/artix-universe/$arch
+Server = https://artixlinux.qontinuum.space/artixlinux/universe/os/$arch
+Server = https://mirror1.cl.netactuate.com/artix/universe/$arch
+Server = https://ftp.crifo.org/artix-universe/$arch
+Server = https://artix.sakamoto.pl/universe/$arch
 
- [omniverse]
- Server = https://eu-mirror.artixlinux.org/omniverse/$arch
- Server = https://omniverse.artixlinux.org/$arch
- Server = https://artix.sakamoto.pl/omniverse/$arch' >> /etc/pacman.conf
+[omniverse]
+Server = https://eu-mirror.artixlinux.org/omniverse/$arch
+Server = https://omniverse.artixlinux.org/$arch
+Server = https://artix.sakamoto.pl/omniverse/$arch' >> /etc/pacman.conf
 
- pacman -Syu artix-archlinux-support
+pacman -Syu artix-archlinux-support
 
- echo -e "
- [extra]
- Include = /etc/pacman.d/mirrorlist-arch
+echo "
+[extra]
+Include = /etc/pacman.d/mirrorlist-arch
 
- [community]
- Include = /etc/pacman.d/mirrorlist-arch
+[community]
+Include = /etc/pacman.d/mirrorlist-arch
 
- [multilib]
- Include = /etc/pacman.d/mirrorlist-arch" >> /etc/pacman.conf
+[multilib]
+Include = /etc/pacman.d/mirrorlist-arch" >> /etc/pacman.conf
 
- pacman-key --populate archlinux
- wget https://github.com/archlinux/svntogit-packages/raw/packages/pacman-mirrorlist/trunk/mirrorlist -O /etc/pacman.d/mirrorlist-arch
+pacman-key --populate archlinux
+wget https://github.com/archlinux/svntogit-packages/raw/packages/pacman-mirrorlist/trunk/mirrorlist -O /etc/pacman.d/mirrorlist-arch
 
- pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
- pacman-key --lsign-key FBA220DFC880C036
- pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key FBA220DFC880C036
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
- echo -e "
- [chaotic-aur]
- Include = /etc/pacman.d/chaotic-mirrorlist\n" >> /etc/pacman.conf
+echo "
+[chaotic-aur]
+Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
 
- pacman -Suy
+pacman -Suy
 
 # finish up
-# exit
+exit
