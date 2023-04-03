7eaf22d72d7fdb21a0bf8b908118a7354bfbbdd1
ArtemNeizhmak <65288888+ArtemNeizhmak@users.noreply.github.com>
2023-04-03 14:32:37 +0000

    error fix
    Bug fix for package installation

--- 1-installer.sh
+++ 1-installer.sh
@@ -10,9 +10,7 @@
  mount /dev/disk/by-label/HOME /mnt/home
 
 # install system
- basestrap /mnt filesystem gcc-libs glibc bash coreutils file findutils gawk grep procps-ng sed tar gettext pciutils \
- psmisc shadow util-linux bzip2 gzip xz licenses pacman artix-keyring seatd-dinit esysusers etmpfiles iputils \
- iproute2 dinit linux linux-firmware grub os-prober dhcpcd wpa_supplicant wpa_supplicant-dinit
+ basestrap /mnt base dinit linux linux-firmware grub os-prober dhcpcd wpa_supplicant wpa_supplicant-dinit
  
  fstabgen -U /mnt >> /mnt/etc/fstab
  cp /2-installer.sh /mnt/
